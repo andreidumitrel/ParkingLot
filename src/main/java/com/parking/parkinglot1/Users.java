62a38ca6c2a5074d34114b5913e6a96b5e7d4662
andreidumitrel <152534347+andreidumitrel@users.noreply.github.com>
2024-11-19 12:38:18 +0200

    Users page is displaying but the list of usersis not shown

---
+++ src/main/java/com/parking/parkinglot1/Users.java
@@ -0,0 +1,28 @@
+package com.parking.parkinglot1;
+
+import com.parking.parkinglot1.common.CarDto;
+import com.parking.parkinglot1.common.UserDto;
+import com.parking.parkinglot1.ejb.CarsBean;
+import com.parking.parkinglot1.ejb.UserBean;
+import jakarta.inject.Inject;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "Users", value = "/Users")
+public class Users extends HttpServlet {
+    @Inject
+    UserBean usersBean;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<UserDto> cars = usersBean.findAllUsers();
+        request.setAttribute("activePage", "Users");
+        request.getRequestDispatcher("/WEB-INF/pages/users.jsp").forward(request,response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse   response) throws ServletException, IOException {
+    }
+}
